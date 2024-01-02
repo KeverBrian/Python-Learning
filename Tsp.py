066421b9dd98dd1e42bee642755dfe88cc0224af
KeverBrian <linjinshengshy@126.com>
2024-01-03 00:16:37 +0800

    稍微完善

--- Tsp.py
+++ Tsp.py
@@ -38,24 +38,35 @@ def nearest_neighbor(distances, start_city):
         min_tour = min(dict_tour.items(), key=lambda item: item[1][1])[0]
         return dict_tour[min_tour]
 
-#二临近算法(未完成)
+#二临近算法(和临近算法相同结果)
 def nearest_twoneighbors(distances, start_city):
     # 初始化
-    tour = [start_city]
-    tour_distance = 0
-    unvisited_cities = set(cities)
-    unvisited_cities.remove(start_city)
-    current_city = start_city
+    forcities = set(cities)
+    forcities.remove(start_city)
+    dict_tour = {}
+    #循环每个邻居城市
+    for i in forcities:
+        current_city = i
+        tour = [start_city]
+        tour_distance = 0
+        unvisited_cities = set(forcities)
+        unvisited_cities.remove(current_city)
+        tour.append(current_city)
     # 逐步访问
-    while unvisited_cities:
-        next_twocities = min(unvisited_cities, key=lambda city: distances[current_city][city]+min(distances[city]))
-        unvisited_cities.remove(next_twocities)
-        tour.append(next_twocities)
-        tour_distance += distances[current_city][next_twocities]
-        current_city = next_twocities
-    tour.append(start_city)
-    tour_distance += distances[current_city][start_city]
-    return tour, tour_distance
+        while unvisited_cities:
+            next_city = min(unvisited_cities, key=lambda city: distances[current_city][city]+min(distances[city]))
+            unvisited_cities.remove(next_city)
+            tour.append(next_city)
+            tour_distance += distances[current_city][next_city]
+            current_city = next_city
+        tour.append(start_city)
+        tour_distance += distances[current_city][start_city]
+        dict_tour[i] = [tour,tour_distance]
+    if len(dict_tour) == 0:
+        return [start_city,0]
+    else:
+        min_tour = min(dict_tour.items(), key=lambda item: item[1][1])[0]
+        return dict_tour[min_tour]
 
 #相对距离算法(和临近算法相同结果)
 def relative_neighbors(distances, start_city):
@@ -111,8 +122,8 @@ tour1 = nearest_neighbor(distances, 0)
 print("Tour_One:", tour1[0], "Total distance:", tour1[1])
 
 #使用二临近算法
-#tour2 = nearest_twoneighbors(distances, 0)
-#print("Tour_Two:", tour2[0], "Total distance:", tour2[1])
+tour2 = nearest_twoneighbors(distances, 0)
+print("Tour_Two:", tour2[0], "Total distance:", tour2[1])
 
 #使用相对距离算法
 tour3 = relative_neighbors(distances, 0)
